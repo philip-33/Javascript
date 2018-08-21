9d7bd21ab40c08241686711b21901e5193e8b292
philip-33 <philip.4242@gmail.com>
2018-08-20 18:45:35 -0700

    break

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,66 @@
-// let firstName = prompt("Hello! What is your first name?");
-// let lastName = prompt("Great! Now, what is your last name?");
-// console.log(`The first name entered is ${firstName}`);
-// alert(`The last name just entered is ${lastName}.`);
-// let month = prompt("What is your birth month? (In numbers, please):")
-// let day = prompt("On what day of that month were you born?:");
-// let year = prompt("And finally, the year?: ");
+/* // Log to the console a string that says "Hello World!"
+//console.log("Hello World!");
 
-// birthday = new Date(year, month-1, day);
-// confirm(`Is ${birthday} your actual birthday?`);
-// alert(`The best birthday ever is ${birthday}.`);
+//Day 2 - Variables and console.log
+//var myName = "Philip";
+//var myAge = 39;
+//var is the ES5 way. Loosely typed, is mutable
+const myName = "Philip";
+let myAge = 39;
+//this is the ES5 way
+//console.log("Hello, " + myName + "! You are " + myAge + " years old.");
+//console.log(myAge);
+
+// Log to the console with the ES6 way
+console.log(`Hello, ${myName}! You are ${myAge} years old.`)
+
+//Day 2 - Date
+const today = new Date();
+let day = today.getDate();
+let month = today.getMonth() + 1; //months start the count at 0
+//let year = today.getYear() + 1900; //years start the count at 1900
+let year = today.getFullYear();
+let hour = today.getHours();
+let minute = today.getMinutes();
+let second = today.getSeconds();
+//console.log(today);
+//console.log("Date: ", day, month, year, " - ", hour, ":", minute, ":", second);
+console.log(`${month}/${day}/${year} - ${hour}:${minute}:${second}`);
+
+//day 2 - Math
+// gets random number between 0 and 1
+//const rand = Math.random();
+
+//give us a random #, multiply by 10. Add 1 to avoid 0, then round down
+const rand = Math.floor((Math.random() * 10) + 1);
+//console.log(rand);
+
+//simple popup
+//alert(rand);
+
+//user clicks cancel or ok
+//confirm(rand);
+
+//accepts a user's input
+let name = prompt('Please enter your name:');
+alert(name);
+//this is a test note for day 2 testing */
+
+
+//day 3
+// === strict comp
+// == lose comp
+// ! not
+// !== strict not comp
+// != loose not comp
+// > GT
+// < LT
+// >= GT or Eq
+// <= LT or Eq
+
+if (1 === 1) {
+    console.log("I am here");
+    //console.log(!true);
+} else {
+    console.log("In the else")
+}
