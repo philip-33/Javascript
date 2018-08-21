3e6e6ca19b5437d5617659a3113fe1848e80d62b
philip-33 <philip.4242@gmail.com>
2018-08-20 18:06:47 -0700

    day 3 initial

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-let firstName = prompt("Hello! What is your first name?");
-let lastName = prompt("Great! Now, what is your last name?");
-console.log(`The first name entered is ${firstName}`);
-alert(`The last name just entered is ${lastName}.`);
-let month = prompt("What is your birth month? (In numbers, please):")
-let day = prompt("On what day of that month were you born?:");
-let year = prompt("And finally, the year?: ");
+// let firstName = prompt("Hello! What is your first name?");
+// let lastName = prompt("Great! Now, what is your last name?");
+// console.log(`The first name entered is ${firstName}`);
+// alert(`The last name just entered is ${lastName}.`);
+// let month = prompt("What is your birth month? (In numbers, please):")
+// let day = prompt("On what day of that month were you born?:");
+// let year = prompt("And finally, the year?: ");
 
-birthday = new Date(year, month-1, day);
-confirm(`Is ${birthday} your actual birthday?`);
-alert(`The best birthday ever is ${birthday}.`);
+// birthday = new Date(year, month-1, day);
+// confirm(`Is ${birthday} your actual birthday?`);
+// alert(`The best birthday ever is ${birthday}.`);
