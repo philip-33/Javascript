b6e29b307a15c82dfb0bde969052d86f20419fa5
philip-33 <philip.4242@gmail.com>
2018-08-22 20:53:36 -0700

    Day 4 complete

--- js/scripts.js
+++ js/scripts.js
@@ -123,12 +123,81 @@ if (1 == userInput) {
 // const division = 10 / 2;
 // const remainder = 11 % 3;
 
-let myNum = 1;
+// let myNum = 1;
 // myNum++; //This incremenr has to be done separately, it can't be done in the console log
 // myNum--; //Dexcrement!
 // console.log(myNum = myNum + 1); //but this is quite all right
-myNum += 3;
-myNum -= 10; //this mechanic is available for all standard arithmetic operators
-myNum *= 5;
-myNum /= 12;
-console.log(myNum);
+// myNum += 3;
+// myNum -= 10; //this mechanic is available for all standard arithmetic operators
+// myNum *= 5;
+// myNum /= 12;
+// console.log(myNum);
+
+// Day 4 - Arrays and Loops
+// let myArray = ['Red', 'Blue', 'Green', 'Purple', 'Black', 'Gray', 'White'];
+// let red = myArray[0]; //dumb, but a functional example
+// console.log(myArray[1]);
+// console.log(myArray.length);
+// let about = ['Phil', 39, 'Going to LA for labor day', true];
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(Array.isArray(nums));
+// adds new to end of array
+// nums.push(10, 11, 12);
+// adds new to the beginning of the array
+// nums.unshift(0, 0.3, 0.5);
+// removes last item of array
+// nums.pop(); //don't pass anything here
+// removes first item of array
+// nums.shift();
+// cuts out section of the array
+// nums.splice(5, 1); // "starting at 5, remove the next 1"
+// nums.splice(1, 4); // "starting at 1, remove the next 4"
+// makes copy of array then cuts the array
+// let slice = nums.slice(3); //take elements after 3, and place them in their own array (thanks to assignment)
+// let slice = nums.slice(0, 5); //creates separate array of 1,2,3,4,5
+// give index of an item in the array
+// let color = myArray.indexOf('Green');
+// // console.log(myArray[color]);
+// myArray.splice(color, 1);
+// let lastIndex = myArray.lastIndexOf('Red'); //returns -1 if it can't find what you are looking for
+// reverse array
+// console.log(lastIndex);
+// console.log(myArray.reverse());
+// sort the array
+// console.log(myArray.sort().reverse());
+
+
+// day 4 - LOOPS
+// let num = 0;
+// while (num < 10) {
+//     //console.log(num);
+//     num++;
+//     console.log(num);
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log('I have ran');
+// }
+// console.log(num);
+
+// do {
+//     num++;
+//     console.log(num);
+// } while (num < 10);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i]);
+// }
+
+let myFavoriteMovies = [];
+let firstFavorite = ['An Affair to Remember', '1957'];
+let secondFavorite = ['Rio Bravo', '1959'];
+let thirdFavorite = ['The Matrix', '1999'];
+myFavoriteMovies.push(firstFavorite, secondFavorite, thirdFavorite);
+// console.log(myFavoriteMovies[2][1]);
+let movieYears = [];
+// movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1], myFavoriteMovies[2][1]);
+for (let i = 0; i < myFavoriteMovies.length; i++) {
+    movieYears.push(myFavoriteMovies[i][1]);
+}
+console.log(movieYears);
