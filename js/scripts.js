d2cdc50d429bead640d564199e567b0944ab894c
philip-33 <philip.4242@gmail.com>
2018-09-05 13:46:26 -0700

    instructions

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// Write a program that does the following:
+
+// When the user clicks a button, prompt them for the a title.
+// Put that title into an h1 that is centered on the page.
+// Ask the user to write a few sentences about what they did today.
+// Populate a p tag with the sentences, align the p tag text with justify.
+// Finally, ask the user for his or her favorite color, and set the background of the p tag to that color
+// Remember, check for edge cases with color.
