1e148ec8123b4a56540dfa15bc4fb6bef287beb0
philip-33 <philip.4242@gmail.com>
2018-09-05 13:43:32 -0700

    Both functions complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,36 @@
 // Write a program that converts temperatures.
-
+let data = ['f', 100];
 // Ask your user to give you a scale (Celcius or Fahrenheit) and the tempurature they want to convert. 
 // So if they give you 90 Fahrenheit, they would want you to convert 90 degrees Fahrenheit to whatever that is in Celcius
- let scale = prompt(`Do you want the scale in Fahrenheit (f) or Celcius? (c)`, 'f');
- let temp = prompt(`Ok, now what temperature do you want to convert in ${scale}?`, 100);
 
  function converter(scale, temp) {
+    let data = [];
+    data[0] = prompt(`Do you want the scale in Fahrenheit (f) or Celcius? (c)`, 'f');
+    data[1] = prompt(`Ok, now what temperature do you want to convert in ${scale}?`, 100);
     // Employ a function that accepts those two values as arguments: 
     // temperature and scale (either celcius or fahrenheit) and converts the tempurature they gave you to the opposite scale.
-    if (scale == 'f') {
-        temp = ((temp - 32) * (5 / 9));
-        scale = 'c';
+    if (data[0] == 'f') {
+        data[1] = ((data[1] - 32) * (5 / 9));
+        data[0] = 'c';
     } else {
-        temp = ((temp * (9/5)) + 32);
-        scale = 'f';
+        data[1] = ((data[1] * (9/5)) + 32);
+        data[0] = 'f';
     }
-    return scale, temp;
+    return data;
  }
 
 
+let tempDiv = document.querySelector(".tempDisplay");
+
+tempDiv.addEventListener(
+    "click",
+    e => {
+        tempArr = [];
+        tempArr = converter(data[0], data[1]);
+        console.log(tempArr);
+        tempDiv.textContent = `The temp is ${tempArr[1]} ${tempArr[0]}.`;
+    }
+)
 // Display the temperature in an h3 with minimal styling.
 
 // From that point on, when the user clicks on the h3, run the function to 
